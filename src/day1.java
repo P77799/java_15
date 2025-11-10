3b3b191591b00ed9403bd92e653e4121cfd989fd
P77799 <mrpratikneplai@gmail.com>
2025-11-10 21:37:00 +0545

    day1

--- src/day1.java
+++ src/day1.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+//public class day1 {
+//    public static void main(String[] args){
+//        System.out.println("hello java programmer");
+//
+//    }
+//}
+import java.util.Scanner;
+public class day1{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String Name=sc.nextLine();
+        System.out.println("Enter your age");
+        String Age=sc.nextLine();
+        System.out.println("my name is " + Name +" i am "+ Age);
 
-public class day1 {
+
+    }
 }
